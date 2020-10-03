85e1e06ba3d458417a2beb615c19a9a792fef41c
Ravi Kawade <rvikwd3@outlook.com>
2020-10-03 01:41:08 -0400

    app8 monica demonstration

--- index.js
+++ index.js
@@ -1,50 +1,16 @@
-var like_button = new Vue({
-    el: '#like_button',
-    data: {
-        message: 'Hello Vue!'
-    }
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{todo.text}}</li>'
 })
 
-var hover_text_date = new Vue({
-    el: '#hover',
+var app8 = new Vue({
+    el: '#app-8',
     data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-})
-
-var disappear = new Vue({
-    el: '#disappear',
-    data: {
-        seen: true
-    }
-})
-
-var list = new Vue({
-    el: '#list',
-    data: {
-        todos: [
-            {text: 'Learn JS'},
-            {text: 'Learn Vue'},
-            {text: 'Build something awesome'}
+        groceryList: [
+            {id: 0, text: 'Vegetables'},
+            {id: 1, text: 'Cheese'},
+            {id: 2, text: 'Whatever else humans eat'}
         ]
-    }
-})
-
-var reverser = new Vue({
-    el: '#reverser',
-    data: {
-        message: 'Hello Vue.js!'
-    },
-    methods:{
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-        }
-    }
-})
-
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-        message: 'Hello Vue!'
     }
 })
